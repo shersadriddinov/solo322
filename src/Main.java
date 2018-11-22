7c9bd09dd2aca78a1124a9b8c2250a3eac9bff8d
Sherzod Sadriddinov <sher.sadriddinov@gmail.com>
2018-11-22 19:44:10 +0500

    Sherzod: Added Bob's chart to project properly

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,6 @@ public class Main {
                     "jdbc:mysql://localhost:3306/javabet", "javaBet", "12345");
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("select Login_Name, Cur_Balance, Gen_Income from accounts");
-            while (rs.next()){
-                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
-            }
-            System.out.println();
             con.close();
         }catch(Exception e){ System.out.println(e);}
     }
