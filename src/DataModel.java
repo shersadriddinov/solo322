01f8ce0aec15fdc455e31d088827db58df4fb964
Sherzod Sadriddinov <sher.sadriddinov@gmail.com>
2018-11-23 20:20:55 +0500

    Sherzod: added switch in main menu

--- src/DataModel.java
+++ src/DataModel.java
@@ -1,20 +1,46 @@
 import java.sql.*;
 import java.lang.*;
 
-public class DataModel{
-    public void insetToAccount(String login){
-        String query = "insert into accounts (Login_Name)" + "values (?)";
-        connectToSQL(query, login);
-    }
-    public void connectToSQL(String query, String login){
+public class DataModel {
+
+    // Accounts table
+    public void insertToAccount(String login) {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javabet", "javaBet", "12345");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javabet",
+                    "javaBet", "12345");
+
+            String query = "insert into accounts (Login_Name)" + "values (?)";
             PreparedStatement pre = connection.prepareStatement(query);
             pre.setString(1, login);
             pre.execute();
+
             connection.close();
-        }catch (Exception e){
+            System.out.println("Successfully added");
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    // bet_history table
+    public void insertToBet_history(String login)
+                                    /*boolean type_bet,
+                                    boolean type_with,
+                                    int sum,
+                                    int cur_balance,
+                                    int gen_income)*/ {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/javabet",
+                    "javaBet", "12345");
+
+            String query = "select ID from accounts" + "where Login_Name = " + login;
+            Statement st = connection.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            System.out.println("Data base returned: ");
+            int id = rs.getInt("1");
+            System.out.println(id);
+        } catch (Exception e){
             System.out.println(e);
         }
     }
--- src/MainController.java
+++ src/MainController.java
@@ -1,13 +1,52 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MainController {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         DataModel data = new DataModel();
-        // Add Login_Name to account
+
+        boolean flag = true;
+        while (flag){
+            int choose;
+            System.out.println("1. To add new account");
+            System.out.println("2. To add new transaction");
+            choose = input.nextInt();
+            switch (choose){
+                case 1:
+                    Login_Name();
+                    flag = false;
+                    break;
+                case 2:
+                    addTransaction();
+                    flag = false;
+                    break;
+                    default:
+                        System.out.println("Enter appropriate integer to choose");
+                        break;
+            }
+        }
+    }
+
+    // Add Login_Name to account
+    public static void Login_Name(){
+        Scanner input = new Scanner(System.in);
+        DataModel data = new DataModel();
+
         System.out.println("Enter login name to create account: ");
         String login;
         login = input.nextLine();
-        data.insetToAccount(login);
+        data.insertToAccount(login);
+    }
+
+    // Add Transactions to account
+    public static void addTransaction(){
+        Scanner input = new Scanner(System.in);
+        DataModel data = new DataModel();
+
+        System.out.println("Enter login: ");
+        String login;
+        login = input.nextLine();
+        data.insertToBet_history(login);
     }
 }
