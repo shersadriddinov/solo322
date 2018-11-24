1e7479e43d5b696c96abd59beb02db2c9f5bd095
Jasur Khakimov <jasur.khakimov.98@mail.ru>
2018-11-24 22:58:01 +0500

    Jasur: readme.txt +

--- python parse/Parsing/logger.py
+++ python parse/Parsing/logger.py
@@ -2,20 +2,19 @@ from selenium import webdriver
 
 # initiate
 driver = webdriver.Chrome()  # initiate a driver, in this case Firefox
-driver.get("https://www.marathonbet.com/su/")  # go to the url
+driver.get(" add url here!!! ")  # go to the url
 
 # locate the login form
 username_field = driver.find_element_by_name("login")  # get the username field
-password_field = driver.find_element_by_name("login_password")  # get the password field
+password_field = driver.find_element_by_name("password")  # get the password field
 
 # log in
-username_field.send_keys("jasurkhakimov")  # enter in your username
-password_field.send_keys("carpediem9")  # enter in your password
-driver.find_element_by_tag_name("button").click()  # submit it
+username_field.send_keys("#your_login")  # enter in your username
+password_field.send_keys("#your_password")  # enter in your password
+driver.find_element_by_name("submit").click()  # submit it
 
 # print HTML
 html = driver.page_source
 print(html)
 
-# 71114211
-# yht73u2a
+
