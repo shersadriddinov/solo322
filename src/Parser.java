a9467841c36a7ce238ee4071ea57e1e87b28a436
U1710070 <bek98@list.ru>
2018-12-11 20:58:08 +0500

    Ula: changes in parser

--- src/Parser.java
+++ src/Parser.java
@@ -5,24 +5,46 @@ import org.apache.poi.xssf.usermodel.*;
 import java.util.Iterator;
 import java.util.*;
 import java.io.*;
+import java.lang.Double;
 public class Parser {
     public static void main(String[] args) throws IOException {
-        File src=new File("EXCEL.xlsx");
-        FileInputStream fis=new FileInputStream(src);
-        XSSFWorkbook wb=new XSSFWorkbook(fis);
-        XSSFSheet sheet1=wb.getSheetAt(0);
+        double  b, c,d;
+        File src = new File("EXCEL.xlsx");
+        FileInputStream fis = new FileInputStream(src);
+        XSSFWorkbook wb = new XSSFWorkbook(fis);
+
+        XSSFSheet sheet1 = wb.getSheetAt(0);
         DataFormatter formatter = new DataFormatter();
-        int rowcount=sheet1.getLastRowNum();
+
+        int rowcount = sheet1.getLastRowNum();
         System.out.println("Total Row " + rowcount);
-        for(int i=0;i<=rowcount;i++) {
-            System.out.println();
-            for (int j = 0; j < 4; j++) {
-                int k=i+1;
-                k=j+1;
-                Cell cell = sheet1.getRow(i).getCell(j);
-                String var_name = formatter.formatCellValue(cell);
-                System.out.print(" " + var_name);
-            }
+
+        for (int i=1; i<28; i++)
+        {
+                Cell cell = sheet1.getRow(i).getCell(0);
+                String a = formatter.formatCellValue(cell);
+                System.out.println(" "+a);
+        }
+        for (int i=1; i<27; i++)
+        {
+            System.out.println(" ");
+            Cell cell1=sheet1.getRow(i).getCell(1);
+            b = cell1.getNumericCellValue();
+            System.out.print(" "+b);
+        }
+        for (int i=1; i<27; i++)
+        {
+            System.out.println(" ");
+            Cell cell2=sheet1.getRow(i).getCell(2);
+            c= new Double(cell2.getNumericCellValue());
+            System.out.print(c+" ");
+        }
+        for (int i=1; i<27 ; i++)
+        {
+            System.out.println(" ");
+            Cell cell3=sheet1.getRow(i).getCell(3);
+            d= cell3.getNumericCellValue();
+            System.out.print(d);
         }
         wb.close();
     }
